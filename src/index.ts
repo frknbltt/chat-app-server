c9e2bc0bb3f074f00281eb6218ff6ea9d9b3ab51
frknbltt <fbulut779@gmail.com>
2022-04-11 09:54:13 +0300

    test

--- src/index.ts
+++ src/index.ts
@@ -37,7 +37,7 @@ app.use("/user", userRoutes);
 app.use("/messages", messageRoutes);
 
 app.use((request: Request, response: Response, next: NextFunction) => {
-  const error = new Error("not founds");
+  const error = new Error("not found");
   return response.status(404).json({
     message: error.message,
   });
